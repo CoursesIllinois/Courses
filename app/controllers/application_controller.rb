fa331dcfccb5a725f10ac939447c6f044e81ee3f
Kiran Ryali <kiran.ryali@gmail.com>
2011-01-05 20:01:02 -0800

    added some comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   protected
-  
+
+  # Retrieve the current user
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
 
+  # Returns whether or not the user is signed in
   def signed_in?
     !!current_user
   end
 
   helper_method :current_user, :signed_in?
 
+  # Helper method to assign and set user in session
   def current_user=(user)
       @current_user = user
       session[:user_id] = user.id
