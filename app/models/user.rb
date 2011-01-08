eb805ed1a09b9516c5971a7232e9d270d80ce8a3
Krishna <kvudata@gmail.com>
2011-01-08 11:13:53 -0500

    Just put a TODO for users, need to remove entries in sections_users when a user is deleted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :sections
+  #TODO add deletion of relation/mapping from users_sections when a user is destroyed
   has_many :authorizations, :dependent => :destroy
   has_one :notify_pref, :dependent => :destroy
   
