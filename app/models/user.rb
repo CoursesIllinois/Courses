d7eb060630c4c32c10bfc203a169c42e78e5ea51
Kiran Ryali <kiran.ryali@gmail.com>
2011-01-14 14:37:00 -0500

    added model helper function to return users unique courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
+
+  def courses
+    courses = []
+    sections.each do |section|
+      course = Course.find(section.course_id)
+      course_name = course.subjectCode + " " + course.courseNumber.to_s
+      unless courses.include?({:name => course_name, :id => section.course_id})
+        courses << {:name => course_name, :id => section.course_id}
+      end
+    end
+    return courses
+  end
+
   has_and_belongs_to_many :sections
   #TODO add deletion of relation/mapping from users_sections when a user is destroyed
   has_many :authorizations, :dependent => :destroy
