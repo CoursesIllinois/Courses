d3d0d8334490a9d0fd79ccb539d5a609f722c8ea
Kiran Ryali <kiran.ryali@gmail.com>
2011-01-22 12:12:16 -0600

    Added function stub

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -1,2 +1,12 @@
 class MessagingController < ApplicationController
+
+  # Handle requests from tropo's server
+
+
+
+  # This function takes in message text and contact from tropo's get request
+  # Generates a json response instructing tropo to deliver the message to the contact           
+  def index 
+  end
+
 end
